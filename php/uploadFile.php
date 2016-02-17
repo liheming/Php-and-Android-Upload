c9c6a10228564a9ca2bda7b1cf563b7f90a17c0e
Rene Gens <renegens@gmail.com>
2016-02-18 00:34:42 +0200

    file script
    working file script

--- php/uploadFile.php
+++ php/uploadFile.php
@@ -1,52 +1,43 @@
 <?php
- 
-// Path to move uploaded files
-$target_path = "uploads/";
- 
-// array for final json respone
-$response = array();
- 
-// getting server ip address
-$server_ip = gethostbyname(gethostname());
- 
-// final file url that is being uploaded
-$file_upload_url = 'http://' . $server_ip . '/' . 'strayanimals.net' . '/' . $target_path;
- 
- 
-if (isset($_FILES['image']['name'])) {
-    $target_path = $target_path . basename($_FILES['image']['name']);
- 
-    // reading other post parameters
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $website = isset($_POST['website']) ? $_POST['website'] : '';
- 
-    $response['file_name'] = basename($_FILES['image']['name']);
-    $response['email'] = $email;
-    $response['website'] = $website;
- 
-    try {
-        // Throws exception incase file is not being moved
-        if (!move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
-            // make error flag true
-            $response['error'] = true;
-            $response['message'] = 'Could not move the file!';
-        }
- 
-        // File successfully uploaded
-        $response['message'] = 'File uploaded successfully!';
-        $response['error'] = false;
-        $response['file_path'] = $file_upload_url . basename($_FILES['image']['name']);
-    } catch (Exception $e) {
-        // Exception occurred. Make error flag true
-        $response['error'] = true;
-        $response['message'] = $e->getMessage();
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["myfile"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["upload"])) {
+    $check = getimagesize($_FILES["myfile"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
-} else {
-    // File parameter is missing
-    $response['error'] = true;
-    $response['message'] = 'Not received any file!F';
 }
- 
-// Echo final json response to client
-echo json_encode($response);
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["myfile"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo "Sorry, only JPG, JPEG, PNG files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["myfile"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 ?>
